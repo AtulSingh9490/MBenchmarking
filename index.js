84247aa8fa3b1e5276761f52661896aae74f950e
Atul Singh <AtulSingh9490@users.noreply.github.com>
2021-01-22 12:00:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = process.env.PORT||3000
+const port = process.env.PORT || 3000
 const express = require('express')
 const app = express()
 
